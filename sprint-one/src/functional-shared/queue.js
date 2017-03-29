24f6c99b696dee53add0e962009bf557ce9889e1
Hack Reactor Student <student@hackreactor.com>
2017-03-29 15:46:37 -0700

    Completes stack and queue implementations for functional-shared classes

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instanceOf = {'count': 0, 'storage': {}};
+  _.extend(instanceOf, queueMethods);
+  return instanceOf;
 };
 
 var queueMethods = {};
-
+queueMethods.enqueue = function(value) {
+	var thisCount = this.count;
+	this.storage[thisCount] = value;
+	this.count++;
+}
+queueMethods.dequeue = function() {
+	var temp;
+	var lowestKey;
+	for (var key in this.storage) {
+		if (lowestKey === undefined || Number(key) < lowestKey) {
+			lowestKey = Number(key);
+		}
+	}
+	temp = this.storage[lowestKey];
+	delete this.storage[lowestKey];
+	return temp;
+};
+queueMethods.size = function() {
+	var amount = 0;
+	for (var key in this.storage) {
+		amount++
+	}
+	return amount;
+}
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
+	var instanceOf = {'count': 0, 'storage': {}};
+
+	_.extend(instanceOf, stackMethods);
+
+	return instanceOf;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value){
+	var thisCount = this.count;
+	this.storage[thisCount] = value;
+	this.count++;
+}
 
+stackMethods.pop = function(){
+	var amount = this.count;
+	var temp;
+	if(this.count > 0) {
+		this.count--;
+		amount -= 1;
+		temp = this.storage[amount];
+		delete this.storage[amount];
+		return temp;	
+	}
+}
+
+stackMethods.size = function(){
+	return this.count;
+}
