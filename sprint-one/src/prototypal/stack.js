1a1195af505b06a09f4b1cc0e12cb994ac4f0e64
Hack Reactor Student <student@hackreactor.com>
2017-03-29 16:42:17 -0700

    Partial implementation of stack for prototypal class

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var instanceOf = Object.create(Stack.prototype);
+  instanceOf.count = 0;
+  instanceOf.storage = {};
+  //debugger;
+  return instanceOf;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value){
+	var thisCount = this.count;
+	this.storage[thisCount] = value;
+	this.count++;
+}
+
+stackMethods.pop = function(){
+	var temp;
+	var amount = this.count;
+
+	if(this.count > 0){
+		this.count--;
+		amount--;
+		temp = this.storage[amount];
+		delete this.storage[amount];
+	}
+	return temp;
+}
+
+stackMethods.size = function(){
+	return this.count;
+}
 
 
