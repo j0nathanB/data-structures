a140eae871b51f824145fa6bcde50c9a7697ba9a
Hack Reactor Student <student@hackreactor.com>
2017-03-31 11:43:19 -0700

    hashTable test in progress

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,45 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, [[k, v]]);
+  } else {
+    for (var i = 0; i<= this._storage.get(index).length; i++) {
+      if (this._storage.get(index)[i][0] === k) {
+        this._storage.get(index)[i][1] = v;
+      };
+    }
+    console.log(this._storage.get(index).push([k,v]))
+  }
+
+  // var temp = this._storage.get(index);
+  // console.log(temp)
+  // if (temp[0][0] === k) {
+  //   console.log(temp);
+  //   temp[0][1] = v;
+  //    console.log("pushed rest");
+  // }else {
+    // console.log('pushed first ', temp);
+    // console.log(this._storage.get(index));
+  // }
+  //console.log(this._storage);
+  // return v;
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var myGet = this._storage.get(index);
+  for (var i = 0; i <= myGet.length - 1; i++) {
+    if (myGet[i][0] === k) {
+      return myGet[i][1];
+    }
+  };
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.get(index).splice(0);
 };
 
 
