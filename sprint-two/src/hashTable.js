6f8e832762e5e6d5adfb5a40937a38ce31be4717
Hack Reactor Student <student@hackreactor.com>
2017-03-31 14:51:27 -0700

    Complete hashTable test

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -11,26 +11,14 @@ HashTable.prototype.insert = function(k, v) {
   if (this._storage.get(index) === undefined) {
     this._storage.set(index, [[k, v]]);
   } else {
-    for (var i = 0; i<= this._storage.get(index).length; i++) {
+    for (var i = 0; i <= this._storage.get(index).length - 1; i++) {
       if (this._storage.get(index)[i][0] === k) {
         this._storage.get(index)[i][1] = v;
       };
     }
-    console.log(this._storage.get(index).push([k,v]))
+    var temp = this._storage.get(index);
+    temp.push([k,v]);
   }
-
-  // var temp = this._storage.get(index);
-  // console.log(temp)
-  // if (temp[0][0] === k) {
-  //   console.log(temp);
-  //   temp[0][1] = v;
-  //    console.log("pushed rest");
-  // }else {
-    // console.log('pushed first ', temp);
-    // console.log(this._storage.get(index));
-  // }
-  //console.log(this._storage);
-  // return v;
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -53,5 +41,4 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
 
